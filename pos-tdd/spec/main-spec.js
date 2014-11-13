010c8c6a12b9315bf89857ab48353098fe01ccdb
NarshaXU <narsha_xu@yahoo.com>
2014-11-13 22:33:33 +0800

    Add the test case of null: if the input is null, the console just show the schemas

--- pos-tdd/spec/main-spec.js
+++ pos-tdd/spec/main-spec.js
@@ -1,5 +1,5 @@
 describe('pos', function () {
-    var allItems, inputs, dateDigitToString;
+    var allItems, inputs, formattedDate;
 
     beforeEach(function () {
         allItems = loadAllItems();
@@ -14,42 +14,26 @@ describe('pos', function () {
             'ITEM000005',
             'ITEM000005'
         ];
-        dateDigitToString = function (num) {
-            return num < 10 ? '0' + num : num;
-        };
+        formattedDate = formateDate();
+
     });
 
-    it('should print correct text', function () {
+    it('should print correct text when input null', function (){
+      spyOn(console, 'log');
 
-        spyOn(console, 'log');
+      var inputTest = [];
+      var expectText =
+          '***<没钱赚商店>购物清单***\n' +
+          '打印时间：' + formattedDate + '\n' +
+          '----------------------\n' +
+          '----------------------\n' +
+          '挥泪赠送商品：\n' +
+          '----------------------\n' +
+          '**********************';
 
-        printInventory(inputs);
+      printInventory(inputTest);
 
-        var currentDate = new Date(),
-            year = dateDigitToString(currentDate.getFullYear()),
-            month = dateDigitToString(currentDate.getMonth() + 1),
-            date = dateDigitToString(currentDate.getDate()),
-            hour = dateDigitToString(currentDate.getHours()),
-            minute = dateDigitToString(currentDate.getMinutes()),
-            second = dateDigitToString(currentDate.getSeconds()),
-            formattedDateString = year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+      expect(console.log).toHaveBeenCalledWith(expectText);
+    })
 
-        var expectText =
-            '***<没钱赚商店>购物清单***\n' +
-            '打印时间：' + formattedDateString + '\n' +
-            '----------------------\n' +
-            '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
-            '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
-            '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
-            '----------------------\n' +
-            '挥泪赠送商品：\n' +
-            '名称：雪碧，数量：1瓶\n' +
-            '名称：方便面，数量：1袋\n' +
-            '----------------------\n' +
-            '总计：51.00(元)\n' +
-            '节省：7.50(元)\n' +
-            '**********************';
-
-        expect(console.log).toHaveBeenCalledWith(expectText);
-    });
 });
---
+++ pos-tdd/src/main.js
@@ -0,0 +1,12 @@
+function printInventory(inputs){
+  var formattedDate = formateDate();
+  var outputStr =
+      '***<没钱赚商店>购物清单***\n' +
+      '打印时间：' + formattedDate + '\n' +
+      '----------------------\n' +
+      '----------------------\n' +
+      '挥泪赠送商品：\n' +
+      '----------------------\n' +
+      '**********************';
+  console.log(outputStr);
+}
